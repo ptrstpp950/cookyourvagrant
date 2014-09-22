5e72bc534ca22f7f34f812b4a91cd61852e84f8b
Piotr Stapp <piotr.stapp@mbank.pl>
2014-09-22 16:24:09 +0200

    Clean up part1

--- js/loadhtmlslides.js
+++ js/loadhtmlslides.js
@@ -1,8 +1,22 @@
+/*global $:false */
+/*global console:false */
 // Modified from markdown.js from Hakim to handle external html files
 (function () {
     /*jslint loopfunc: true, browser: true*/
     /*globals alert*/
     'use strict';
+
+    String.prototype.format = String.prototype.f = function() {
+        var s = this,
+            i = arguments.length;
+
+        while (i--) {
+            s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+        }
+        return s;
+    };
+
+    var templateGithub = '&nbsp;<br/><span class="github-btn github-watchers github-btn-large"><a class="gh-btn active" href="https://github.com/ptrstpp950/{0}/commit/{1}" target="_blank"><span class="gh-ico"></span> <span class="gh-text" id="gh-text">{1}</span></a></span>';
 
     var querySlidingHtml = function () {
         var sections = document.querySelectorAll('[data-html]'),
@@ -22,6 +36,16 @@
                                 xhr.status === 0 // file protocol yields status code 0 (useful for local debug, mobile applications etc.)
                                 ) {
                                 section.innerHTML = xhr.responseText;
+                                if($(section).find('.commit').length>0){
+                                    $(section).find('.commit').each(function() {
+                                        var commit = $(this).attr('data-commit');
+                                        var repo = $(this).attr('data-repo');
+                                        this.innerHTML = templateGithub.format(repo,commit);
+                                        console.log(templateGithub.format(repo,commit));
+
+                                    });
+                                }
+                                
                             } else {
                                 section.outerHTML = '<section data-state="alert">ERROR: The attempt to fetch ' + url + ' failed with the HTTP status ' + xhr.status + '. Check your browser\'s JavaScript console for more details.</p></section>';
                             }
